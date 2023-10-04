21197720831a37b8990e77226bd3f083b1b724a0
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-04 16:00:42 +0530

    Added Game logic

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -8,7 +8,23 @@
 
 // document.querySelector('.guess').value=12;
 // console.log(document.querySelector('.guess').value);
+const secretNumber=Math.trunc(Math.random()*20)+1;
+// console.log(secretNumber);
+document.querySelector('.number').textContent=secretNumber;
+let score=20;
 document.querySelector('.check').addEventListener('click', function(){
-  console.log(document.querySelector('.guess').value);
-
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent=`⛔No number!`;
+  }else if (guess===secretNumber) {
+    document.querySelector('.message').textContent= `🎉Correct number!`;
+  }else if (guess>secretNumber) {
+    document.querySelector('.message').textContent=`📈Too high`;
+    score--;
+    document.querySelector('.score').textContent=score;
+  }else if (guess<secretNumber){
+    document.querySelector('.message').textContent=`📉Too low`;
+    score--;
+    document.querySelector('.score').textContent=score;
+  }
 });
