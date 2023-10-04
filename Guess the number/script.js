dc0dfa4abd290799dc6eacf804aab2e5a29e103c
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-04 18:18:46 +0530

    Added high score functionality and cleaned the code

--- Guess the number/script.js
+++ Guess the number/script.js
@@ -1,10 +1,10 @@
 "use strict";
+// Game data
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
-let highScore=0;
+let highScore = 0;
 
-// GAME LOGIC 
+// GAME LOGIC
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
@@ -12,33 +12,26 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent = `⛔No number!`;
 
-    // When player wins
+
+// When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = `🎉Correct number!`;
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "green";
-    document.querySelector('.number').style.width="30rem";
-    highScore=score;
-    document.querySelector('.highscore').textContent= highScore;
-
-    // When pnumber is greater
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = `📈Too high`;
-      score--;
-      
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = `💩You lose`;
-      document.querySelector(".score").textContent = 0;
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
     }
 
-    //When number is smaller
-  } else if (guess < secretNumber) {
+
+    // When player loses
+  } else if (guess != secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = `📉Too low`;
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? `📈Too high` : `📉Too low`;
       score--;
-    
+
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = `💩You lose`;
@@ -47,16 +40,17 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 });
 
+
+
 //Reset functionality
-document.querySelector('.again').addEventListener('click', function(){
-  document.querySelector('.number').textContent=`?`;
-  document.querySelector('.number').style.width="15rem";
-  document.querySelector('body').style.backgroundColor="#222";
-  score=20;
-  highScore=0;
-  document.querySelector('.score').textContent=score;
-  document.querySelector('.highscore').textContent=highScore;
-  document.querySelector('.message').textContent=`Start guessing...`;
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".number").textContent = `?`;
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".highscore").textContent = highScore;
+  document.querySelector(".message").textContent = `Start guessing...`;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.guess').value="";
+  document.querySelector(".guess").value = "";
 });
