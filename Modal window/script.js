78bc62212751e20bed7e62bd81f55b9d40872609
Faizan Ashraf7 <faixannajar71@gmail.com>
2023-10-05 09:07:24 +0530

    Added all the needed vairiables

--- Modal window/script.js
+++ Modal window/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Assign the classes to the variables 
+const btnsShow= document.querySelectorAll('.show-modal');
+const modal= document.querySelector('.modal');
+const overlay= document.querySelector('.overlay');
+const btnClose= document.querySelector('colse-modal');
+console.log(btnsShow);
+for (let i = 0; i < btnsShow.length; i++) {
+  console.log(btnsShow[i].textContent);
+  
+}
